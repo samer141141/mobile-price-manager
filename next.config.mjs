b58c50256fb84455b204ed0d8f541685118ebac6
samer141141 <samerabouamra@gmail.com>
2026-09-23 14:44:49 +0200

    Provide market API build environment to server routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    TRADERA_APP_ID: process.env.TRADERA_APP_ID,
+    TRADERA_APP_KEY: process.env.TRADERA_APP_KEY,
+    PRISJAKT_CLIENT_ID: process.env.PRISJAKT_CLIENT_ID,
+    PRISJAKT_CLIENT_SECRET: process.env.PRISJAKT_CLIENT_SECRET,
+    PRISJAKT_REF_ID: process.env.PRISJAKT_REF_ID,
+  },
+};
 
 export default nextConfig;
